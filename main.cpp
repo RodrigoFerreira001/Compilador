47f6574ef20ebbebccec22be09a908e65fe3848f
danilodna <danillodna@gmail.com>
2016-08-17 12:35:21 -0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,20 @@
-#include "readFile.hpp"
+#include "Buffer.hpp"
 
 int main(int argc, char** argv){
 
-	if(argc < 2){
+	if(argc < 2) {
 		printf("Invalid parameters.\n");
 		exit(EXIT_FAILURE);
 	}
+	stringstream aux(argv[1]);
+	int bufferSize;
+	aux >> bufferSize;
 
-	string* buffer = readFile(argv[1]);
+	Buffer buffer(bufferSize);
+	buffer.load(argv[2]);
+	//buffer.print();
 
-	cout << *buffer << endl;
+	
 
 	return 0;
 }
