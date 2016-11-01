fcf17136218d8d372735ac63b5e481aa09493d15
RodrigoFerreira001 <rodrigoferreira001@hotmail.com>
2016-11-01 18:46:39 -0200

    E LA VAMOS NOS

--- SyntaxAnalyzer/SyntaxAnalyzer.cpp
+++ SyntaxAnalyzer/SyntaxAnalyzer.cpp
@@ -1,19 +1,58 @@
 #include "SyntaxAnalyzer.hpp"
 
-void SyntaxAnalyzer::print_syntactic_error(Token token){
-	if(token.getToken() == "ID" || token.getNome() == "NUMBER"){
-		printf(“%s esperado na linha %d”, token.getNome(), token.getNumLinha());
+void SyntaxAnalyzer::print_syntactic_error(Token* token){
+	if(token->getToken() == "ID" || token->getToken() == "NUMBER"){
+		cout << token->getTableEntry()->getToken() << " esperado na linha " <<
+		token->getTableEntry()->getLineNumber() << endl;
 	}else{
-		printf(“%s esperado na linha %d”, token.getLexema(), token.getNumLinha());
+		cout << token->getTableEntry()->getLexeme() << " esperado na linha " <<
+		token->getTableEntry()->getLineNumber() << endl;
 	}
 }
 
-bool SyntaxAnalyzer::match(Token token, int index, vector<Token*>* token_vector){
+bool synchronize(Token* token, int& index, vector<Token*>* token_vector){
+	return true;
+}
 
+bool SyntaxAnalyzer::match(Token* token, int& index, vector<Token*>* token_vector){
+	if(token_vector->at(index).getToken() == token->getToken()){
+		i++;
+	}else{
+		print_syntactic_error(token);
+		return synchronize();
+	}
 }
 
 vector<Declaration> SyntaxAnalyzer::declarations_list(vector<Token*>* token_vector, int index){
-
+	int currentType = -1;
+    if(w[i] == INT || w[i] == FLOAT)
+    {
+        List<Declaracao> declList = new ArrayList();
+        currentType = Tipo(); ///0 = INT, 1 = FLOAT
+        if(w[i] == ID)
+        {
+            w[i].getEntry().setType(currentType);
+            w[i].getEntry().setIsVarDecl(true);
+            /**
+            A classe Temp gera um novo temporário a cada instanciação. O número de temporários é ilimitado.
+            Um temporário é uma abstração para um registrador ou para uma posição de memória.
+            */
+            Temp temp = new Temp();///o nome do novo temporário é definido no construtor
+            Declaracao id = new Declaracao(w[i].getEntry(),temp);
+            w[i].getEntry().setTemp(temp);
+            declList.add(id);
+            match(ID);
+        }
+        else
+        {
+            imprimeErro(ID);
+            sincroniza(ID);
+        }
+        List<Declaracao> declList2 = Decl2(currentType);
+        declList.add(declList2);
+        return declList;
+    }
+    else return nullptr; ///else não faz nada Declaracoes -> e
 }
 
 int SyntaxAnalyzer::type(vector<Token*>* token_vector, int index){
--- SyntaxAnalyzer/SyntaxAnalyzer.hpp
+++ SyntaxAnalyzer/SyntaxAnalyzer.hpp
@@ -10,8 +10,8 @@ private:
 	AbstractSyntaxTree* ast;
 	vector<Token*>* token_vector;
 
-	void print_syntactic_error(Token token);
-	bool match(Token token, int index, vector<Token*>* token_vector);
+	void print_syntactic_error(Token* token);
+	bool match(Token* token, int& index, vector<Token*>* token_vector);
 	vector<Declaration> declarations_list(vector<Token*>* token_vector, int index);
 	int type(vector<Token*>* token_vector, int index);
 
